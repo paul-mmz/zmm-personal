c1b5b46bded06a1b981294cce4ac63b276dd7b4c
hzzhouminmin <hzzhouminmin@corp.netease.com>
2017-02-17 20:20:12 +0800

    misc:提交咯

--- src/main/java/com/paul/zmm_personal/App.java
+++ src/main/java/com/paul/zmm_personal/App.java
@@ -3,21 +3,30 @@ package com.paul.zmm_personal;
 public class App {
 
 	public static void main(String[] args) {
-		ResultInfoObject<KaolaContact> resultInfo = null;
-		resultInfo = new ResultInfoObject<>(200, "success");
-		resultInfo.setResult(new Object());
-		if (resultInfo.getObject() == null) {
-			KaolaContact kaolaContact = new KaolaContact();
-			kaolaContact.setId(1000L);
-			resultInfo.setObject(kaolaContact);
-		}
-		Long contactNewId1 = (resultInfo != null && resultInfo.getObject() != null
-				&& resultInfo.getObject().getId() != null) ? resultInfo.getObject().getId() : 0L;
-
-		System.out.println(contactNewId1);
-		Long contactNewId2 = (resultInfo == null || resultInfo.getObject() == null
-				|| resultInfo.getObject().getId() == null) ? 0L : resultInfo.getObject().getId();
-		System.out.println("everything's fine: " + contactNewId2);
+//		ResultInfoObject<KaolaContact> resultInfo = null;
+//		resultInfo = new ResultInfoObject<>(200, "success");
+//		resultInfo.setResult(new Object());
+//		if (resultInfo.getObject() == null) {
+//			KaolaContact kaolaContact = new KaolaContact();
+//			kaolaContact.setId(1000L);
+//			resultInfo.setObject(kaolaContact);
+//		}
+//		Long contactNewId1 = (resultInfo != null && resultInfo.getObject() != null
+//				&& resultInfo.getObject().getId() != null) ? resultInfo.getObject().getId() : 0L;
+//
+//		System.out.println(contactNewId1);
+//		Long contactNewId2 = (resultInfo == null || resultInfo.getObject() == null
+//				|| resultInfo.getObject().getId() == null) ? 0L : resultInfo.getObject().getId();
+//		System.out.println("everything's fine: " + contactNewId2);
+	    
+	    try {
+	        String s = "this is a emssage";
+	        System.out.println(s);
+	        throw new RuntimeException("runtime exception");
+	    }catch (Exception e){
+//	        System.out.println(e.getMessage());
+//	        System.out.println("wo jiu bu chu li");
+	    }
 
 	}
 }
