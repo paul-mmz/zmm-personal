19023f59706f98c0b6432d7a4334e40100c2cab5
hzzhouminmin <hzzhouminmin@corp.netease.com>
2017-04-27 16:27:10 +0800

    提交

--- src/main/java/com/paul/zmm_personal/App.java
+++ src/main/java/com/paul/zmm_personal/App.java
@@ -1,8 +1,14 @@
 package com.paul.zmm_personal;
 
+import java.util.UUID;
+
 public class App {
 
 	public static void main(String[] args) {
 	    System.out.println("Hello World");
+	    
+	    UUID uuid = UUID.randomUUID();
+	    System.out.println(uuid);
+	    
 	}
 }
