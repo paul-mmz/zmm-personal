4a2c2f2aa660cc564e3dbaf5848b96178e6cb149
hzzhouminmin <hzzhouminmin@corp.netease.com>
2017-06-25 02:33:36 +0800

    update study

--- src/main/java/com/paul/zmm_personal/App.java
+++ src/main/java/com/paul/zmm_personal/App.java
@@ -4,12 +4,16 @@ import java.util.UUID;
 
 public class App {
 
-	public static void main(String... args) {
-	    System.out.println("Hello World");
-	    
-	    UUID uuid = UUID.randomUUID();
-	    System.out.println(uuid);
-	    
-	}
-	
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid);
+        for (char c : "THIS IS A StRING".toCharArray()) {
+            System.out.println(c);
+        }
+        System.out.println(args.getClass().getName().toString());
+        System.out.println("STRING".getClass().getName().toString());
+    }
 }
