88b38f4b05a2c7463bb38a8deefa75433203444e
hzzhouminmin <hzzhouminmin@corp.netease.com>
2018-01-04 23:44:36 +0800

    commit

--- src/main/java/com/paul/sort/Main.java
+++ src/main/java/com/paul/sort/Main.java
@@ -5,22 +5,45 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(9);
-        list.add(8);
-        list.add(7);
-        list.add(6);
-        list.add(5);
-        list.add(4);
-        list.add(3);
-        list.add(2);
-        list.add(1);
-        list.add(0);
-        System.out.println(list);
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(9);
+        list1.add(8);
+        list1.add(7);
+        list1.add(6);
+        list1.add(5);
+        list1.add(4);
+        list1.add(3);
+        list1.add(2);
+        list1.add(1);
+        list1.add(0);
+        System.out.println(list1);
 
-        QuickSort.quickSort(list, 2);
+//        QuickSort.quickSort(list1, 2);
 
-        System.out.println(list);
+        HeapSort.heapSort(list1);
+
+        System.out.println(list1);
+
+        System.out.println("******************");
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(0);
+        list2.add(1);
+        list2.add(2);
+        list2.add(3);
+        list2.add(4);
+        list2.add(5);
+        list2.add(6);
+        list2.add(7);
+        list2.add(8);
+        list2.add(9);
+        System.out.println(list2);
+
+//        QuickSort.quickSort(list2, 2);
+
+        HeapSort.heapSort(list2);
+
+        System.out.println(list2);
 
     }
 }
