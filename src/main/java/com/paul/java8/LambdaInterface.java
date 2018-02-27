7060749b503255d159d147fdceeff164aabbd9c7
hzzhouminmin <hzzhouminmin@corp.netease.com>
2018-02-27 21:18:32 +0800

    commit

---
+++ src/main/java/com/paul/java8/LambdaInterface.java
@@ -0,0 +1,10 @@
+package com.paul.java8;
+
+/**
+ * Created by hzzhouminmin on 2018/2/27.
+ */
+@FunctionalInterface
+public interface LambdaInterface {
+
+    public String myprint(String s);
+}
--- src/main/java/com/paul/java8/Main.java
+++ src/main/java/com/paul/java8/Main.java
@@ -1,33 +1,61 @@
 package com.paul.java8;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        new Thread(()-> System.out.println("new Thread")).start();
 
-        Runnable runna = Main::f;
-        Thread thread = new Thread(runna);
-        thread.start();
-        thread.join();
+    public static void lambda1(LambdaInterface lambdaInterface) {
+        new Thread(() -> System.out.print("new Thread")).start();
+        new Thread(() -> {System.out.print("new Thread");}).start();
 
-        List<String> strList = new ArrayList<String>() {
-            {
-                add("nihao");
-                add("jjsf");
-                add("psvm");
+        System.out.println(lambdaInterface.myprint("yyyyyyyyys"));
+
+        Collections.sort(Arrays.asList(1,2), (Integer x, Integer y) -> {
+            if (x > y) {
+                return 1;
             }
-        };
+            return -1;
+        });
 
-        Collections.sort(strList, String::compareToIgnoreCase);
-        System.out.println(strList);
+        lambdaInterface = (String s) -> {return s;};
+        Predicate<String> sp1 = (String s) -> s.startsWith("j");
+        Predicate<String> sp2 = (String s) -> s.startsWith("j");
+        sp1.and(sp2);
+    }
 
+    public static void lambda2() {
+        List<String> list = Arrays.asList("hello", "world", "ni", "hao");
+        list.forEach((String s) -> {
+            System.out.println(s);
+        });
+
+        list.forEach(System.out::println);
 
     }
 
-    public static void f() {
-        System.out.println("hello, i'm function");
+    public static void stream() {
+        List<Integer> integers = Arrays.asList(1,2,3,4,6);
+        integers.forEach(System.out::println);
+        integers.stream().forEach((Integer i) -> System.out.println(i));
+        integers.stream().map((Integer i) -> {return i*i;}).forEach(System.out::println);
+        Integer su = integers.stream().map((Integer i) -> {
+            return i * i;
+        }).reduce((sum, cost) -> sum + cost).get();
+        System.out.println(su);
     }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        lambda1((String s) -> {System.out.println(s); return s;});
+
+        lambda2();
+
+        stream();
+
+    }
+
 }
