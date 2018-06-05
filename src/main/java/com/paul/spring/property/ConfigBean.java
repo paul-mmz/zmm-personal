900ca74ba09db8fdf3df8ed79b1d9d568dc8dbbb
hzzhouminmin <hzzhouminmin@corp.netease.com>
2018-06-06 00:56:10 +0800

    commit

--- src/main/java/com/paul/spring/property/ConfigBean.java
+++ src/main/java/com/paul/spring/property/ConfigBean.java
@@ -1,22 +1,41 @@
 package com.paul.spring.property;
 
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 @Configuration
 @ComponentScan
 @ImportResource("classpath:properties.xml")
-@PropertySource("classpath:message.properties")
-@PropertySource("classpath:config/application.properties")
+//@PropertySource("classpath:message.properties")
+//@PropertySource("classpath:config/application.properties")
 public class ConfigBean {
 
-    @Bean
-    public PropertiesFactoryBean getPropertiesFactory() {
-        PropertiesFactoryBean bean = new PropertiesFactoryBean();
-        ClassPathResource source = new ClassPathResource("message.properties");
-        bean.setLocations(source);
-        return bean;
-    }
+//    @Bean
+//    public PropertiesFactoryBean getPropertiesFactory() {
+//        PropertiesFactoryBean bean = new PropertiesFactoryBean();
+//        ClassPathResource source = new ClassPathResource("message.properties");
+//        bean.setLocations(source);
+//        return bean;
+//    }
+
+//    @Bean
+//    public BeanFactoryPostProcessor getPropertyPlaceholderConfigurer() {
+//        PropertyPlaceholderConfigurer configurer = new PropertyPlaceholderConfigurer();
+//        ClassPathResource source = new ClassPathResource("message.properties");
+//        configurer.setLocations(source);
+//        return configurer;
+//    }
+
+//    @Bean
+//    public BeanFactoryPostProcessor getPropertySourcesPlaceholderConfigurer() {
+//        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+//        ClassPathResource source = new ClassPathResource("message.properties");
+//        configurer.setLocations(source);
+//        return configurer;
+//    }
 }
--- src/main/java/com/paul/spring/property/Main.java
+++ src/main/java/com/paul/spring/property/Main.java
@@ -2,21 +2,22 @@ package com.paul.spring.property;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.env.Environment;
 
 
 public class Main {
     public static void main(String[] args) {
-//        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:properties.xml");
-        ApplicationContext context = new AnnotationConfigApplicationContext(ConfigBean.class);
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:properties.xml");
+//        ApplicationContext context = new AnnotationConfigApplicationContext(ConfigBean.class);
         Environment environment = context.getEnvironment();
 
 
-        System.out.println(environment.getProperty("user.host"));
+//        System.out.println(environment.getProperty("user.host"));
 
         TestBean bean = (TestBean)context.getBean("testBean");
         System.out.println(bean.getHost());
-
+        System.out.println(bean.getPasswd());
 
     }
 }
