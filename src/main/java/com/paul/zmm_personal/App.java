f25a09bdd63fd389ddc519345475f71af0147e2e
hzzhouminmin <hzzhouminmin@corp.netease.com>
2018-07-27 10:08:42 +0800

    commit

--- src/main/java/com/paul/zmm_personal/App.java
+++ src/main/java/com/paul/zmm_personal/App.java
@@ -4,13 +4,4 @@ public class App {
 
 	public static void main(String[] args) {
 	}
-
-	public static void f() {
-		System.out.println("nihao");
-	}
-
-	public static void ff() {
-		System.out.println("ff");
-	}
-
 }
