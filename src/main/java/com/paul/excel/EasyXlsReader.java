0cd6cdcd1f696d6953c2b38e0e3c7125951dd996
hzzhouminmin <hzzhouminmin@corp.neteasec.om>
2018-12-17 17:54:41 +0800

    easyExcel

--- src/main/java/com/paul/excel/EasyXlsReader.java
+++ src/main/java/com/paul/excel/EasyXlsReader.java
@@ -3,6 +3,7 @@ package com.paul.excel;
 import com.alibaba.excel.ExcelReader;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
+import com.alibaba.fastjson.JSON;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -10,26 +11,9 @@ import java.util.List;
 
 public class EasyXlsReader {
     public static void main(String[] args) throws FileNotFoundException {
-        InputStream inputStream = new BufferedInputStream(new FileInputStream(new File("C:/Users/hzzhouminmin/Desktop/地址库.xlsx")));
-//        InputStream inputStream = ClassLoader.getSystemResourceAsStream("地址库.xlsx");
-
-        List<Object> contents = new ArrayList<>();
-
-        ExcelReader excelReader = new ExcelReader(inputStream, null, new AnalysisEventListener<Object>() {
-
-            @Override
-            public void invoke(Object object, AnalysisContext context) {
-                contents.add(object);
-            }
-
-            @Override
-            public void doAfterAllAnalysed(AnalysisContext context) {
-                System.out.println("finish excel reader");
-            }
-        });
-
-        excelReader.read();
-
-        System.out.println(contents);
+        List<Long> bizs = new ArrayList<>();
+        bizs.add(1000L);
+        bizs.add(2000L);
+        System.out.println(JSON.toJSONString(bizs));
     }
 }
